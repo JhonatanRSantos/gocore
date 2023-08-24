e880493baedf20806b56a532744ed76f6817d881
Jhonatan R. Santos <jhonatanatm@gmail.com>
2023-08-23 23:19:10 -0300

    Add GoLog pkg (#4)

--- go.mod
+++ go.mod
@@ -2,4 +2,37 @@ module github.com/JhonatanRSantos/gocore
 
 go 1.19
 
-require golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
+require (
+	go.uber.org/zap v1.25.0
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
+	gopkg.in/DataDog/dd-trace-go.v1 v1.54.0
+)
+
+require (
+	github.com/DataDog/appsec-internal-go v1.0.0 // indirect
+	github.com/DataDog/datadog-agent/pkg/obfuscate v0.45.0-rc.1 // indirect
+	github.com/DataDog/datadog-agent/pkg/remoteconfig/state v0.48.0-devel.0.20230725154044-2549ba9058df // indirect
+	github.com/DataDog/datadog-go/v5 v5.1.1 // indirect
+	github.com/DataDog/go-libddwaf v1.4.2 // indirect
+	github.com/DataDog/go-tuf v1.0.1-0.5.2 // indirect
+	github.com/DataDog/sketches-go v1.2.1 // indirect
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/ebitengine/purego v0.5.0-alpha // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/outcaste-io/ristretto v0.2.1 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/secure-systems-lab/go-securesystemslib v0.7.0 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20220617031537-928513b29760 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	inet.af/netaddr v0.0.0-20220811202034-502d2d690317 // indirect
+)
---
+++ pkg/golog/golog.go
@@ -0,0 +1,281 @@
+// Provides a set of tools and syntax sugar for printing logs.
+// Warning, this package is designed to work with Datadog. We're not using OTEL conventions.
+package golog
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/JhonatanRSantos/gocore/pkg/goenv"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
+
+type (
+	logType string
+	Options func(ctx context.Context, message string, logger *Logger, logType logType)
+)
+
+var (
+	logInfo  logType = "info"
+	logWarn  logType = "warn"
+	logDebug logType = "debug"
+	logError logType = "error"
+
+	logColorRed    = "\033[31m"
+	logColorGreen  = "\033[32m"
+	logColorYellow = "\033[33m"
+	logColorBlue   = "\033[34m"
+
+	logEnvironment = goenv.Test
+
+	logWriter           = os.Stdout
+	logger      *Logger = nil
+	initLogOnce sync.Once
+)
+
+type Logger struct {
+	infoLogger  *log.Logger
+	warnLogger  *log.Logger
+	debugLogger *log.Logger
+	errorLogger *log.Logger
+	zapLogger   *zap.Logger
+}
+
+func SetEnv(env goenv.Env) {
+	logEnvironment = env
+}
+
+func Log() *Logger {
+	init := func() {
+		if logger == nil {
+			if logEnvironment == goenv.Test || logEnvironment == goenv.Local {
+				logger = &Logger{
+					infoLogger:  log.New(logWriter, fmt.Sprintf("%v[  INFO  ] ", logColorGreen), log.LUTC),
+					warnLogger:  log.New(logWriter, fmt.Sprintf("%v[  WARN  ] ", logColorYellow), log.LUTC),
+					debugLogger: log.New(logWriter, fmt.Sprintf("%v[  DEBUG ] ", logColorBlue), log.LUTC),
+					errorLogger: log.New(logWriter, fmt.Sprintf("%v[  ERROR ] ", logColorRed), log.LUTC),
+				}
+			} else {
+				var err error
+				zapConfig := zap.Config{
+					Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
+					Development: true,
+					Encoding:    "json",
+					EncoderConfig: zapcore.EncoderConfig{
+						TimeKey:        "timestamp",
+						LevelKey:       "status",
+						NameKey:        zapcore.OmitKey,
+						CallerKey:      zapcore.OmitKey,
+						FunctionKey:    zapcore.OmitKey,
+						MessageKey:     "message",
+						StacktraceKey:  zapcore.OmitKey,
+						LineEnding:     zapcore.DefaultLineEnding,
+						EncodeLevel:    zapcore.CapitalLevelEncoder,
+						EncodeTime:     zapcore.TimeEncoderOfLayout(time.RFC3339),
+						EncodeDuration: zapcore.StringDurationEncoder,
+						EncodeCaller:   zapcore.ShortCallerEncoder,
+					},
+					OutputPaths:      []string{"stderr"},
+					ErrorOutputPaths: []string{"stderr"},
+				}
+
+				logger = &Logger{}
+
+				if logEnvironment == goenv.Development || logEnvironment == goenv.Staging {
+					logger.zapLogger, err = zapConfig.Build()
+				} else {
+					zapConfig.Level = zap.NewAtomicLevelAt(zap.WarnLevel)
+					zapConfig.Development = false
+					logger.zapLogger, err = zapConfig.Build()
+				}
+
+				if err != nil {
+					panic(fmt.Errorf("failed to load logger configs. Cause: %s", err))
+				}
+				zap.ReplaceGlobals(logger.zapLogger.Sugar().Desugar())
+			}
+		}
+	}
+	initLogOnce.Do(init)
+	return logger
+}
+
+// WithTags Add a group of tags into current log. Tags are used to provide more context when writing logs.
+func WithTags(tags map[string]interface{}) Options {
+	return func(ctx context.Context, message string, logger *Logger, logType logType) {
+		allTags := []interface{}{}
+		if logEnvironment == goenv.Test || logEnvironment == goenv.Local {
+			bs, _ := json.MarshalIndent(tags, "", "\t")
+			allTags = append(allTags, string(bs))
+		} else {
+			allTags = parseTags("", tags)
+		}
+
+		switch logType {
+		case logInfo:
+			logger.infoLog(ctx, message, allTags...)
+		case logWarn:
+			logger.warnLog(ctx, message, allTags...)
+		case logDebug:
+			logger.debugLog(ctx, message, allTags...)
+		case logError:
+			logger.errorLog(ctx, message, allTags...)
+		default:
+			logger.errorLogger.Println("- invalid log type")
+		}
+	}
+}
+
+// parseTags Convert a map of tags to a slice of tags that will be usad by zap
+func parseTags(name string, tags map[string]interface{}) []interface{} {
+	allTags := []interface{}{}
+	for tagKey, tagValue := range tags {
+		tagFullName := fmt.Sprintf("%s.%s", name, tagKey)
+
+		if name == "" {
+			tagFullName = tagKey
+		}
+
+		if subTags, isSubTags := tagValue.(map[string]interface{}); !isSubTags {
+			value := tagValue
+			if strings.ContainsAny(fmt.Sprint(value), "[]") {
+				value = strings.Join(strings.Fields(fmt.Sprint(value)), ", ")
+			}
+
+			allTags = append(allTags, fmt.Sprintf("tags.%s", tagFullName), value)
+		} else {
+			allTags = append(allTags, parseTags(tagFullName, subTags)...)
+		}
+	}
+	return allTags
+}
+
+// getTracerAndSpanID Get the tracer and span id
+func getTracerAndSpanID(ctx context.Context) (traceID uint64, spanID uint64) {
+	if ctx == nil {
+		return traceID, spanID
+	}
+
+	defer func() {
+		_ = recover()
+	}()
+
+	if span, ok := tracer.SpanFromContext(ctx); ok {
+		return span.Context().TraceID(), span.Context().SpanID()
+	}
+
+	return 0, 0
+}
+
+// Info Prints using the info level
+func (l *Logger) Info(ctx context.Context, message string, opts ...Options) {
+	if len(opts) > 0 {
+		for _, op := range opts {
+			op(ctx, message, l, logInfo)
+		}
+	} else {
+		l.infoLog(ctx, message)
+	}
+}
+
+func (l *Logger) infoLog(ctx context.Context, message string, tags ...interface{}) {
+	if logEnvironment == goenv.Test || logEnvironment == goenv.Local {
+		if len(tags) > 0 {
+			l.infoLogger.Printf("- %s\n%v\n", message, tags)
+		} else {
+			l.infoLogger.Printf("- %s\n", message)
+		}
+	} else {
+		if traceID, spanID := getTracerAndSpanID(ctx); traceID != 0 && spanID != 0 {
+			tags = append(tags, "dd.trace_id", traceID, "dd.span_id", spanID)
+		}
+		l.zapLogger.Sugar().Infow(message, tags...)
+	}
+}
+
+// Warn Prints using the warn level
+func (l *Logger) Warn(ctx context.Context, message string, opts ...Options) {
+	if len(opts) > 0 {
+		for _, op := range opts {
+			op(ctx, message, l, logWarn)
+		}
+	} else {
+		l.warnLog(ctx, message)
+	}
+}
+
+func (l *Logger) warnLog(ctx context.Context, message string, tags ...interface{}) {
+	if logEnvironment == goenv.Test || logEnvironment == goenv.Local {
+		if len(tags) > 0 {
+			l.warnLogger.Printf("- %s\n%v\n", message, tags)
+		} else {
+			l.warnLogger.Printf("- %s\n", message)
+		}
+	} else {
+		if traceID, spanID := getTracerAndSpanID(ctx); traceID != 0 && spanID != 0 {
+			tags = append(tags, "dd.trace_id", traceID, "dd.span_id", spanID)
+		}
+		l.zapLogger.Sugar().Warnw(message, tags...)
+	}
+}
+
+// Debug Prints using the debug level
+func (l *Logger) Debug(ctx context.Context, message string, opts ...Options) {
+	if len(opts) > 0 {
+		for _, op := range opts {
+			op(ctx, message, l, logDebug)
+		}
+	} else {
+		l.debugLog(ctx, message)
+	}
+}
+
+func (l *Logger) debugLog(ctx context.Context, message string, tags ...interface{}) {
+	if logEnvironment == goenv.Test || logEnvironment == goenv.Local {
+		if len(tags) > 0 {
+			l.debugLogger.Printf("- %s\n%v\n", message, tags)
+		} else {
+			l.debugLogger.Printf("- %s\n", message)
+		}
+	} else {
+		if traceID, spanID := getTracerAndSpanID(ctx); traceID != 0 && spanID != 0 {
+			tags = append(tags, "dd.trace_id", traceID, "dd.span_id", spanID)
+		}
+		l.zapLogger.Sugar().Debugw(message, tags...)
+	}
+}
+
+// Error Prints using the error level
+func (l *Logger) Error(ctx context.Context, message string, opts ...Options) {
+	if len(opts) > 0 {
+		for _, op := range opts {
+			op(ctx, message, l, logError)
+		}
+	} else {
+		l.errorLog(ctx, message)
+	}
+}
+
+func (l *Logger) errorLog(ctx context.Context, message string, tags ...interface{}) {
+	if logEnvironment == goenv.Test || logEnvironment == goenv.Local {
+		if len(tags) > 0 {
+			l.errorLogger.Printf("- %s\n%v\n", message, tags)
+		} else {
+			l.errorLogger.Printf("- %s\n", message)
+		}
+	} else {
+		if traceID, spanID := getTracerAndSpanID(ctx); traceID != 0 && spanID != 0 {
+			tags = append(tags, "dd.trace_id", traceID, "dd.span_id", spanID)
+		}
+		l.zapLogger.Sugar().Errorw(message, tags...)
+	}
+}
